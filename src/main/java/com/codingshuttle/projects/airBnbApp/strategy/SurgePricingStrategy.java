f5ee88ff29d1736b231f35efd385b6259ecdeac8
Govind Thakur <thakurgovind1998@gmail.com>
2026-08-05 21:28:42 +0530

    Dynamic pricing feature

---
+++ src/main/java/com/codingshuttle/projects/airBnbApp/strategy/SurgePricingStrategy.java
@@ -0,0 +1,17 @@
+package com.codingshuttle.projects.airBnbApp.strategy;
+
+import com.codingshuttle.projects.airBnbApp.entity.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+@Service
+@RequiredArgsConstructor
+public class SurgePricingStrategy implements PricingStrategy {
+    private final PricingStrategy wrapped;
+    @Override
+    public BigDecimal calculatePrice(Inventory inventory) {
+        BigDecimal price=wrapped.calculatePrice(inventory);
+        return price.multiply(inventory.getSurgeFactor());
+    }
+}
